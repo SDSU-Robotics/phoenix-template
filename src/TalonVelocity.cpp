673861abd13d050a834ace4178eb9849b29c0544
Ryan Fick <Ryan.fick@jacks.sdstate.edu>
2020-03-07 14:37:22 -0600

    "Most recent constants"

--- src/TalonVelocity.cpp
+++ src/TalonVelocity.cpp
@@ -7,8 +7,6 @@
 #include "ros/ros.h"
 #include "std_msgs/Float32.h"
 
-#include <iostream>
-
 using namespace std;
 using namespace ctre::phoenix;
 using namespace ctre::phoenix::platform;
@@ -20,7 +18,7 @@ using namespace ctre::phoenix::motorcontrol::can;
 #define GEAR_RATIO 26.9
 #define COUNTS_PER_REVOLUTION 7
 
-#define MAX_SPEED 1.0
+#define MAX_SPEED 3.0
 
 double mps2countsPer100ms(double mps)
 {
@@ -56,7 +54,6 @@ int main (int argc, char **argv)
 	Listener listener;
 
 	ros::Subscriber speed_sub = n.subscribe("speed", 1000, &Listener::setSpeed, &listener);
-
 	ros::Publisher actualSpeed_pub = n.advertise<std_msgs::Float32>("actualSpeed", 100);
 
 	std_msgs::Float32 actualSpeed_msg;
@@ -109,22 +106,21 @@ Listener::Listener()
 	motorProfile.primaryPID.selectedFeedbackCoefficient = 1.0f;//0.25f;// 0.328293f;
 
 	//PID Constants
-	motorProfile.slot0.kP                       = 20.0f; //0.01f; //Propotional Constant.  Controls the speed of error correction.
-	motorProfile.slot0.kI                       = 0.0f; //Integral Constant.     Controls the steady-state error correction.
-	motorProfile.slot0.kD                       = 10.0f; //Derivative Constant.   Controls error oscillation.
+	motorProfile.slot0.kP                       = 10.0f; //0.01f; //Propotional Constant.  Controls the speed of error correction.
+	motorProfile.slot0.kI                       = 0.001f; //Integral Constant.     Controls the steady-state error correction.
+	motorProfile.slot0.kD                       = 0.1f; //Derivative Constant.   Controls error oscillation.
 	motorProfile.slot0.kF                       = 0.0f; //Feed Forward Constant. (IDK what this does)
 	motorProfile.slot0.integralZone             = 100000;   //Maximum value for the integral error accumulator. Automatically cleared when exceeded.
 	motorProfile.slot0.maxIntegralAccumulator   = 10000;   //Maximum value for the integral error accumulator. (IDK what this does)
-	motorProfile.slot0.allowableClosedloopError = 2.0;   //If the total error-value is less than this value, the error is automatically set to zero.
+	motorProfile.slot0.allowableClosedloopError = 0.001;   //If the total error-value is less than this value, the error is automatically set to zero.
 	motorProfile.slot0.closedLoopPeakOutput     = 1.0f; //Peak output for the PID Controller.
-	motorProfile.slot0.closedLoopPeriod         = 500;   //Samples per second (?) (IDK what this is)
+	motorProfile.slot0.closedLoopPeriod         = 100;   //Samples per second (?) (IDK what this is)
 
 	_motor.ConfigAllSettings(motorProfile);
 
 	_motor.SetNeutralMode(NeutralMode::Brake);
 	_motor.SetInverted(false);
 	_motor.SetSensorPhase(false);
-
 }
 
 
