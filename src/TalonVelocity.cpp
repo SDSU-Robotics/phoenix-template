6deb085f2d2bc8c8173cba71d88ba7e3612193fd
Ryan Fick <Ryan.fick@jacks.sdstate.edu>
2020-03-07 11:46:35 -0600

    Added actual speed feedback

--- src/TalonVelocity.cpp
+++ src/TalonVelocity.cpp
@@ -22,9 +22,14 @@ using namespace ctre::phoenix::motorcontrol::can;
 
 #define MAX_SPEED 1.0
 
-double mps2counts(double mps)
+double mps2countsPer100ms(double mps)
 {
 	return mps*0.1*(1.0/(DIAMETER*PI))*GEAR_RATIO*COUNTS_PER_REVOLUTION;
+}
+
+double countsPer100ms2mps(double countsPer100ms)
+{
+	return countsPer100ms/(0.1*(1.0/(DIAMETER*PI))*GEAR_RATIO*COUNTS_PER_REVOLUTION);
 }
 
 
@@ -33,6 +38,7 @@ class Listener
 public:
 	Listener();
 	void setSpeed(const std_msgs::Float32 msg);
+	double getActualSpeed();
 
 private:
 	TalonSRX _motor = {DeviceIDs::motor};
@@ -43,6 +49,7 @@ int main (int argc, char **argv)
 {
 	ros::init(argc, argv, "TalonVelocity");
 	ros::NodeHandle n;
+	ros::Rate loop_rate(100);
 	
 	ctre::phoenix::platform::can::SetCANInterface("can0");
 
@@ -50,7 +57,19 @@ int main (int argc, char **argv)
 
 	ros::Subscriber speed_sub = n.subscribe("speed", 1000, &Listener::setSpeed, &listener);
 
-	ros::spin();
+	ros::Publisher actualSpeed_pub = n.advertise<std_msgs::Float32>("actualSpeed", 100);
+
+	std_msgs::Float32 actualSpeed_msg;
+
+	while (ros::ok())
+	{
+		actualSpeed_msg.data = listener.getActualSpeed();
+		
+		actualSpeed_pub.publish(actualSpeed_msg);
+		
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
 
 	return 0;
 }
@@ -65,9 +84,7 @@ void Listener::setSpeed(const std_msgs::Float32 msg)
 	else if (mps > MAX_SPEED)
 		mps = MAX_SPEED;
 
-	cout << mps2counts(mps) << endl;
-	cout << _motor.GetSelectedSensorVelocity() << endl;
-	_motor.Set(ControlMode::Velocity, mps2counts(mps));
+	_motor.Set(ControlMode::Velocity, mps2countsPer100ms(mps));
 
 	ctre::phoenix::unmanaged::FeedEnable(100); // feed watchdog
 }
@@ -92,13 +109,13 @@ Listener::Listener()
 	motorProfile.primaryPID.selectedFeedbackCoefficient = 1.0f;//0.25f;// 0.328293f;
 
 	//PID Constants
-	motorProfile.slot0.kP                       = 100.0f; //0.01f; //Propotional Constant.  Controls the speed of error correction.
+	motorProfile.slot0.kP                       = 20.0f; //0.01f; //Propotional Constant.  Controls the speed of error correction.
 	motorProfile.slot0.kI                       = 0.0f; //Integral Constant.     Controls the steady-state error correction.
-	motorProfile.slot0.kD                       = 0.0f; //Derivative Constant.   Controls error oscillation.
+	motorProfile.slot0.kD                       = 10.0f; //Derivative Constant.   Controls error oscillation.
 	motorProfile.slot0.kF                       = 0.0f; //Feed Forward Constant. (IDK what this does)
 	motorProfile.slot0.integralZone             = 100000;   //Maximum value for the integral error accumulator. Automatically cleared when exceeded.
 	motorProfile.slot0.maxIntegralAccumulator   = 10000;   //Maximum value for the integral error accumulator. (IDK what this does)
-	motorProfile.slot0.allowableClosedloopError = 0.0;   //If the total error-value is less than this value, the error is automatically set to zero.
+	motorProfile.slot0.allowableClosedloopError = 2.0;   //If the total error-value is less than this value, the error is automatically set to zero.
 	motorProfile.slot0.closedLoopPeakOutput     = 1.0f; //Peak output for the PID Controller.
 	motorProfile.slot0.closedLoopPeriod         = 500;   //Samples per second (?) (IDK what this is)
 
@@ -110,3 +127,8 @@ Listener::Listener()
 
 }
 
+
+double Listener::getActualSpeed()
+{
+	return countsPer100ms2mps(_motor.GetSelectedSensorVelocity());
+}
