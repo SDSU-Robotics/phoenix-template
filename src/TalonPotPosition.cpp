2825247dfc221011b8fcba7a14084b5da7a2473e
Tyler Fogelson <tyler.fogelson@jacks.sdstate.edu>
2020-03-16 17:05:15 -0500

    Pot position modeled after TalonVelocity

--- src/TalonPotPosition.cpp
+++ src/TalonPotPosition.cpp
@@ -5,81 +5,67 @@
 #include "DeviceIDs.h"
 
 #include "ros/ros.h"
-#include "std_msgs/Int64.h"
-
-#include "ctre/Phoenix.h"
-#include "ctre/phoenix/platform/Platform.h"
-#include "ctre/phoenix/unmanaged/Unmanaged.h"
-
-#include "std_msgs/Float64.h"
+#include "std_msgs/Float32.h"
 
 using namespace std;
 using namespace ctre::phoenix;
 using namespace ctre::phoenix::platform;
 using namespace ctre::phoenix::motorcontrol;
 using namespace ctre::phoenix::motorcontrol::can;
-//using namespace ctre::phoenix::motorcontrol::TalonSRXFeedbackDevice;
 
-const int MAX_RPM = 2000;
+#define PI 3.1415927
+#define DIAMETER 0.2		// should be updated to non-tread diameter of wheels
+#define GEAR_RATIO 26.9
+#define COUNTS_PER_REVOLUTION 7
+
+#define MAX_SPEED 3.0
+
+double mps2countsPer100ms(double mps)
+{
+	return mps*0.1*(1.0/(DIAMETER*PI))*GEAR_RATIO*COUNTS_PER_REVOLUTION;
+}
+
+double countsPer100ms2mps(double countsPer100ms)
+{
+	return countsPer100ms/(0.1*(1.0/(DIAMETER*PI))*GEAR_RATIO*COUNTS_PER_REVOLUTION);
+}
+
 
 class Listener
 {
 public:
 	Listener();
-	void setRPM(const std_msgs::Float64 msg);
-	void setSP(const std_msgs::Int64 msg);
-	void setIntake(const std_msgs::Float64 msg);
-	void setCommencement(const std_msgs::Float64 msg);
-	void updateLinAct();
+	void setSpeed(const std_msgs::Float32 msg);
+	double getActualPosition();
 
-	TalonSRX _comArm = {DeviceIDs::commencementArm};
-	TalonSRX _linAct = {DeviceIDs::launcherAngle};
-	TalonSRX _intakeLeft = {DeviceIDs::intakeLeft};
-	TalonSRX _intakeRight = {DeviceIDs::intakeRight};
-
-	float _rpmSetpoint = 0.0;
-
-	PIDController _SPController;
-	int _lastSP = 0;
+private:
+	TalonSRX _motor = {DeviceIDs::motor};
 };
 
 
 int main (int argc, char **argv)
 {
-    ctre::phoenix::platform::can::SetCANInterface("can1");
-    Listener listener;
+	ros::init(argc, argv, "TalonPotPosition");
+	ros::NodeHandle n;
+	ros::Rate loop_rate(100);
+	
+	ctre::phoenix::platform::can::SetCANInterface("can0");
 
-	ros::Subscriber set_RPM_sub = n.subscribe("set_RPM", 1000, &Listener::setRPM, &listener);
-	ros::Subscriber set_intake_sub = n.subscribe("set_intake", 1000, &Listener::setIntake, &listener);
-	ros::Subscriber set_commencement_sub = n.subscribe("set_commencement", 1000, &Listener::setCommencement, &listener);
+	Listener listener;
 
-	ros::Publisher pos_pub = n.advertise<std_msgs::Float64>("PotPos", 1000);
-	ros::Publisher sp_pub = n.advertise<std_msgs::Float64>("Pot_setpoint", 1000);
+	ros::Subscriber speed_sub = n.subscribe("speed", 1000, &Listener::setSpeed, &listener);
+	ros::Publisher actualPosition_pub = n.advertise<std_msgs::Float32>("actualPosition", 100);
 
-	std_msgs::Float64 pos_msg;
-	std_msgs::Float64 sp_msg;
+	std_msgs::Float32 actualPosition_msg;
 
-	listener._linAct.SetSelectedSensorPosition(0);
-	listener._linAct.Set(ControlMode::Position, 0);
-	listener._lastSP = 0;
-	listener._ActSPController.setKP(1);
-	listener._ActSPController.setMaxOut(100);
+	
 
 	while (ros::ok())
 	{
-		RPM_msg.data = Conversions::toRpm( listener._linAct.GetSelectedSensorVelocity() );
-
-		RPM_pub.publish(top_RPM_msg);
-
-		pos_msg.data = listener._linAct.GetSelectedSensorPosition() / 4096.0 / 100.0 / 85.0 * 42.0 * 360.0 + 34.5;
-		pos_pub.publish(pos_msg);
-
-		listener.updatelinAct();
-		sp_msg.data = listener._lastSP / 4096.0 / 100.0 / 85.0 * 42.0 * 360.0 + 34.5;
-		sp_pub.publish(sp_msg);
-
-		ctre::phoenix::unmanaged::FeedEnable(100); // feed watchdog
-
+		actualPosition_msg.data = listener.getActualPosition();
+		
+		actualPosition_pub.publish(actualPosition_msg);
+		
 		ros::spinOnce();
 		loop_rate.sleep();
 	}
@@ -88,166 +74,62 @@ int main (int argc, char **argv)
 }
 
 
+void Listener::setSpeed(const std_msgs::Float32 msg)
+{
+	// limit values
+	float mps = msg.data;
+	if (mps < -MAX_SPEED)
+		mps = -MAX_SPEED;
+	else if (mps > MAX_SPEED)
+		mps = MAX_SPEED;
+
+	_motor.Set(ControlMode::Velocity, mps2countsPer100ms(mps));
+
+	ctre::phoenix::unmanaged::FeedEnable(100); // feed watchdog
+}
+
+
 Listener::Listener()
 {
-	// // ============================== Top Wheel ==============================
-	// TalonSRXConfiguration topProfile;
+	TalonSRXConfiguration motorProfile;
 
-	// //Threshold for zero-motion for the neutral position.
-	// topProfile.neutralDeadband = 0.01;
-			
-	// //Peak Speed Config
-	// topProfile.peakOutputForward = 1.0;
-	// topProfile.peakOutputReverse = -1.0;
-	
-	// //Ramp Config
-	// topProfile.closedloopRamp = 1.5f;
-			
-	// //PID Config
-	// topProfile.primaryPID.selectedFeedbackSensor = FeedbackDevice::Analog;
-	// topProfile.primaryPID.selectedFeedbackCoefficient = 1.0f;//0.25f;// 0.328293f;
-
-	// //PID Constants
-	// topProfile.slot0.kP                       = 0.025f; //0.01f; //Propotional Constant.  Controls the speed of error correction.
-	// topProfile.slot0.kI                       = 0.0165f; //Integral Constant.     Controls the steady-state error correction.
-	// topProfile.slot0.kD                       = 0.016f; //Derivative Constant.   Controls error oscillation.
-	// topProfile.slot0.kF                       = 0.0084f; //Feed Forward Constant. (IDK what this does)
-	// topProfile.slot0.integralZone             = 100000;   //Maximum value for the integral error accumulator. Automatically cleared when exceeded.
-	// topProfile.slot0.maxIntegralAccumulator   = 10000;   //Maximum value for the integral error accumulator. (IDK what this does)
-	// topProfile.slot0.allowableClosedloopError = 217;   //If the total error-value is less than this value, the error is automatically set to zero.
-	// topProfile.slot0.closedLoopPeakOutput     = 1.0f; //Peak output for the PID Controller.
-	// topProfile.slot0.closedLoopPeriod         = 500;   //Samples per second (?) (IDK what this is)
-
-	// TalonSRXPIDSetConfiguration profile;
-
-	// _linAct.ConfigAllSettings(topProfile);
-
-	// _linAct.SetNeutralMode(NeutralMode::Brake);
-	// _linAct.SetInverted(false);
-	// _linAct.SetSensorPhase(false);
-
-	// // ============================== Bottom Wheel ==============================
-
-	// TalonSRXConfiguration bottomProfile;
-
-	// //Threshold for zero-motion for the neutral position.
-	// bottomProfile.neutralDeadband = 0.01;
-			
-	// //Peak Speed Config
-	// bottomProfile.peakOutputForward = 1.0;
-	// bottomProfile.peakOutputReverse = -1.0;
-			
-	// //Ramp Config
-	// bottomProfile.closedloopRamp = 1.5f;
-			
-	// //PID Config
-	// bottomProfile.primaryPID.selectedFeedbackSensor = FeedbackDevice::QuadEncoder;
-	// bottomProfile.primaryPID.selectedFeedbackCoefficient = 1.0f;//0.25f;// 0.328293f;
-
-	// //PID Constants
-	// bottomProfile.slot0.kP                       = 0.025f; //0.01f; //Propotional Constant.  Controls the speed of error correction.
-	// bottomProfile.slot0.kI                       = 0.0165f; //Integral Constant.     Controls the steady-state error correction.
-	// bottomProfile.slot0.kD                       = 0.016f; //Derivative Constant.   Controls error oscillation.
-	// bottomProfile.slot0.kF                       = 0.0084; //Feed Forward Constant. For velocity
-	// bottomProfile.slot0.integralZone             = 100000;   //Maximum value for the integral error accumulator. Automatically cleared when exceeded.
-	// bottomProfile.slot0.maxIntegralAccumulator   = 10000;   //Maximum value for the integral error accumulator. Biggest Error for I
-	// bottomProfile.slot0.allowableClosedloopError = 217;   //If the total error-value is less than this value, the error is automatically set to zero.
-	// bottomProfile.slot0.closedLoopPeakOutput     = 1.0f; //Peak output for the PID Controller.
-	// bottomProfile.slot0.closedLoopPeriod         = 500;   //Samples per second (?) (IDK what this is)
-
-	// _bottomWheel.ConfigAllSettings(bottomProfile);
-
-	// _bottomWheel.SetNeutralMode(NeutralMode::Brake);
-	// _bottomWheel.SetInverted(false);
-	// _bottomWheel.SetSensorPhase(true);
-
-
-	// // =========================== Commencement Arm =============================
-	 _comArm.SetInverted(true);
-
-	// ============================== Angle Motor ===============================
-	TalonSRXConfiguration linActProf;
+	ros::Duration(1.0).sleep();    // give time to finish initialization before trying to read position data, otherwise it will fail
 
 	//Threshold for zero-motion for the neutral position.
-	linActProf.neutralDeadband = 0.01;
+	motorProfile.neutralDeadband = 0.01;
 			
 	//Peak Speed Config
-	linActProf.peakOutputForward = 0.5;
-	linActProf.peakOutputReverse = -0.5;
-			
+	motorProfile.peakOutputForward = 1.0;
+	motorProfile.peakOutputReverse = -1.0;
+	
 	//Ramp Config
-	linActProf.closedloopRamp = 1.5f;
+	motorProfile.closedloopRamp = 1.5f;
 			
 	//PID Config
-	linActProf.primaryPID.selectedFeedbackSensor = FeedbackDevice::Analog;
-	linActProf.primaryPID.selectedFeedbackCoefficient = 1.0f;//0.25f;// 0.328293f;
+	//motorProfile.primaryPID.selectedFeedbackSensor = FeedbackDevice::QuadEncoder;
+	motorProfile.primaryPID.selectedFeedbackSensor = FeedbackDevice::Analog;
+	motorProfile.primaryPID.selectedFeedbackCoefficient = 1.0f;//0.25f;// 0.328293f;
 
 	//PID Constants
-	linActProf.slot0.kP                       = 0.01; //0.01f; //Propotional Constant.  Controls the speed of error correction.
-	linActProf.slot0.kI                       = 0.006; //Integral Constant.     Controls the steady-state error correction.
-	linActProf.slot0.kD                       = 0.06; //Derivative Constant.   Controls error oscillation.
-	linActProf.slot0.kF                       = 0.0; //Feed Forward Constant. For velocity
-	linActProf.slot0.integralZone             = 100000;   //Maximum value for the integral error accumulator. Automatically cleared when exceeded.
-	linActProf.slot0.maxIntegralAccumulator   = 10000;   //Maximum value for the integral error accumulator. Biggest Error for I
-	linActProf.slot0.allowableClosedloopError = 217;   //If the total error-value is less than this value, the error is automatically set to zero.
-	linActProf.slot0.closedLoopPeakOutput     = 0.3f; //Peak output for the PID Controller.
-	linActProf.slot0.closedLoopPeriod         = 500;   //Samples per second (?) (IDK what this is)
+	motorProfile.slot0.kP                       = 10.0f; //0.01f; //Propotional Constant.  Controls the speed of error correction.
+	motorProfile.slot0.kI                       = 0.001f; //Integral Constant.     Controls the steady-state error correction.
+	motorProfile.slot0.kD                       = 0.1f; //Derivative Constant.   Controls error oscillation.
+	motorProfile.slot0.kF                       = 0.0f; //Feed Forward Constant. (IDK what this does)
+	motorProfile.slot0.integralZone             = 100000;   //Maximum value for the integral error accumulator. Automatically cleared when exceeded.
+	motorProfile.slot0.maxIntegralAccumulator   = 10000;   //Maximum value for the integral error accumulator. (IDK what this does)
+	motorProfile.slot0.allowableClosedloopError = 0.001;   //If the total error-value is less than this value, the error is automatically set to zero.
+	motorProfile.slot0.closedLoopPeakOutput     = 1.0f; //Peak output for the PID Controller.
+	motorProfile.slot0.closedLoopPeriod         = 100;   //Samples per second (?) (IDK what this is)
 
-	_linAct.ConfigAllSettings(linActProf);
+	_motor.ConfigAllSettings(motorProfile);
 
-	_linAct.SetNeutralMode(NeutralMode::Brake);
-	_linAct.SetInverted(true);
-	_linAct.SetSensorPhase(true);
+	_motor.SetNeutralMode(NeutralMode::Brake);
+	_motor.SetInverted(false);
+	_motor.SetSensorPhase(false);
 }
 
-
-void Listener::setRPM(const std_msgs::Float64 msg)
+// reads speed of motor shaft and returns in meters/sec
+double Listener::getActualPosition()
 {
-	float rpm = msg.data;
-	if (rpm > 0.01)
-	{
-		if (rpm > MAX_RPM)
-			rpm = MAX_RPM;
-		
-		_rpmSetpoint = rpm;
-		_linAct.Set(ControlMode::Velocity, Conversions::fromRpm(rpm - 100));
-	}
-	else
-	{
-		_rpmSetpoint = 0.0;
-		_linAct.Set(ControlMode::PercentOutput, 0.0);
-	}
-}
-
-void Listener::SetSP(const std_msgs::Int64 msg)
-{
-	if (msg.data < 0)
-		_linAct.SetSelectedSensorPosition(0);
-	else
-		_linActSPController.setSetpoint(msg.data);
-}
-
-void Listener::updatelinAct()
-{
-	_lastSP = _lastSP + _linActSPController.calcOutput(_lastSP);
-	_linAct.Set(ControlMode::Position, _lastSP);
-}
-
-void Listener::setIntake(const std_msgs::Float64 msg)
-{
-	_intakeLeft.Set(ControlMode::PercentOutput, -1 * msg.data);
-	_intakeRight.Set(ControlMode::PercentOutput, msg.data);
-}
-
-void Listener::setCommencement(const std_msgs::Float64 msg)
-{
-	#ifndef COMMENCEMENT
-		return;
-	#endif
-	_comArm.Set(ControlMode::PercentOutput, msg.data);
-}
-
-
-
-
+	return _motor.GetSelectedSensorVelocity();
 }
--- src/TalonVelocity.cpp
+++ src/TalonVelocity.cpp
@@ -13,10 +13,30 @@ using namespace ctre::phoenix::platform;
 using namespace ctre::phoenix::motorcontrol;
 using namespace ctre::phoenix::motorcontrol::can;
 
+#define PI 3.1415927
+#define DIAMETER 0.2		// should be updated to non-tread diameter of wheels
+#define GEAR_RATIO 26.9
+#define COUNTS_PER_REVOLUTION 7
+
+#define MAX_SPEED 3.0
+
+double mps2countsPer100ms(double mps)
+{
+	return mps*0.1*(1.0/(DIAMETER*PI))*GEAR_RATIO*COUNTS_PER_REVOLUTION;
+}
+
+double countsPer100ms2mps(double countsPer100ms)
+{
+	return countsPer100ms/(0.1*(1.0/(DIAMETER*PI))*GEAR_RATIO*COUNTS_PER_REVOLUTION);
+}
+
+
 class Listener
 {
 public:
+	Listener();
 	void setSpeed(const std_msgs::Float32 msg);
+	double getActualSpeed();
 
 private:
 	TalonSRX _motor = {DeviceIDs::motor};
@@ -25,16 +45,30 @@ private:
 
 int main (int argc, char **argv)
 {
-	ros::init(argc, argv, "Talon Percent Output");
+	ros::init(argc, argv, "TalonVelocity");
 	ros::NodeHandle n;
+	ros::Rate loop_rate(100);
 	
 	ctre::phoenix::platform::can::SetCANInterface("can0");
 
 	Listener listener;
 
 	ros::Subscriber speed_sub = n.subscribe("speed", 1000, &Listener::setSpeed, &listener);
+	ros::Publisher actualSpeed_pub = n.advertise<std_msgs::Float32>("actualSpeed", 100);
 
-	ros::spin();
+	std_msgs::Float32 actualSpeed_msg;
+
+	
+
+	while (ros::ok())
+	{
+		actualSpeed_msg.data = listener.getActualSpeed();
+		
+		actualSpeed_pub.publish(actualSpeed_msg);
+		
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
 
 	return 0;
 }
@@ -43,13 +77,58 @@ int main (int argc, char **argv)
 void Listener::setSpeed(const std_msgs::Float32 msg)
 {
 	// limit values
-	float percentOutput = msg.data;
-	if (percentOutput < -1.0f)
-		percentOutput = -1.0f;
-	else if (percentOutput > 1.0f)
-		percentOutput = 1.0f;
+	float mps = msg.data;
+	if (mps < -MAX_SPEED)
+		mps = -MAX_SPEED;
+	else if (mps > MAX_SPEED)
+		mps = MAX_SPEED;
 
-	_motor.Set(ControlMode::PercentOutput, percentOutput);
+	_motor.Set(ControlMode::Velocity, mps2countsPer100ms(mps));
 
 	ctre::phoenix::unmanaged::FeedEnable(100); // feed watchdog
+}
+
+
+Listener::Listener()
+{
+	TalonSRXConfiguration motorProfile;
+
+	ros::Duration(1.0).sleep();    // give time to finish initialization before trying to read position data, otherwise it will fail
+
+	//Threshold for zero-motion for the neutral position.
+	motorProfile.neutralDeadband = 0.01;
+			
+	//Peak Speed Config
+	motorProfile.peakOutputForward = 1.0;
+	motorProfile.peakOutputReverse = -1.0;
+	
+	//Ramp Config
+	motorProfile.closedloopRamp = 1.5f;
+			
+	//PID Config
+	motorProfile.primaryPID.selectedFeedbackSensor = FeedbackDevice::QuadEncoder;
+	motorProfile.primaryPID.selectedFeedbackCoefficient = 1.0f;//0.25f;// 0.328293f;
+
+	//PID Constants
+	motorProfile.slot0.kP                       = 10.0f; //0.01f; //Propotional Constant.  Controls the speed of error correction.
+	motorProfile.slot0.kI                       = 0.001f; //Integral Constant.     Controls the steady-state error correction.
+	motorProfile.slot0.kD                       = 0.1f; //Derivative Constant.   Controls error oscillation.
+	motorProfile.slot0.kF                       = 0.0f; //Feed Forward Constant. (IDK what this does)
+	motorProfile.slot0.integralZone             = 100000;   //Maximum value for the integral error accumulator. Automatically cleared when exceeded.
+	motorProfile.slot0.maxIntegralAccumulator   = 10000;   //Maximum value for the integral error accumulator. (IDK what this does)
+	motorProfile.slot0.allowableClosedloopError = 0.001;   //If the total error-value is less than this value, the error is automatically set to zero.
+	motorProfile.slot0.closedLoopPeakOutput     = 1.0f; //Peak output for the PID Controller.
+	motorProfile.slot0.closedLoopPeriod         = 100;   //Samples per second (?) (IDK what this is)
+
+	_motor.ConfigAllSettings(motorProfile);
+
+	_motor.SetNeutralMode(NeutralMode::Brake);
+	_motor.SetInverted(false);
+	_motor.SetSensorPhase(false);
+}
+
+// reads speed of motor shaft and returns in meters/sec
+double Listener::getActualSpeed()
+{
+	return countsPer100ms2mps(_motor.GetSelectedSensorVelocity());
 }
